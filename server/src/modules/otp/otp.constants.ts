88cb955c6b7db92febf4071b0427bac96a0d4d1a
AkkalDhami <dhamiakkal21@gmail.com>
2026-09-15 16:22:03 +0545

    feat(otp): add constants for OTP configuration
    - Define maximum OTP attempts as 5
    - Set spam lock time to 1 hour
    - Specify OTP code length as 6 digits
    - Set OTP expiration time to 5 minutes
    - Add cooldown period of 1 minute between OTP requests
    - Define delay before next OTP request as 1 minute in milliseconds

---
+++ server/src/modules/otp/otp.constants.ts
@@ -0,0 +1,8 @@
+export const OTP_MAX_ATTEMPTS = 5
+export const OTP_SPAM_LOCK_TIME = 1 * 60 * 60 // 1 hour
+export const OTP_CODE_LENGTH = 6 as const
+export const OTP_EXPIRES_IN = 5 * 60 // 5 minutes
+export const OTP_COOL_DOWN = 1 * 60 // 1 minute
+
+export const NEXT_OTP_DELAY = 1 * 60 * 1000 // 1 minute
+// export const OTP_EXPIRES_IN_MS = 5 * 60 * 1000 // 5 minutes
